2f11fe1c37d5209f992b4ba60c5b034a99388958
hezhi <zhi.he@aihuishou.com>
2021-05-28 13:53:53 +0800

    feat:create-react-app引入less

--- src/page/test/index.tsx
+++ src/page/test/index.tsx
@@ -1,43 +1,37 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import './index.less'
-import { Button , Typography} from 'antd'
+import * as React from "react";
+import { useState, useEffect } from "react";
+import "./index.less";
+import { Button, Typography } from "antd";
 
-export interface Props {
-    
-}
-const {Paragraph}=Typography
- 
+export interface Props {}
+const { Paragraph } = Typography;
+
 const Test: React.FC<Props> = () => {
+  const Restart = () => {};
 
-    const Restart = () => {
+  const Next = () => {};
 
-    };
-  
-    const Next = () => {};
-  
-    const Back = () => {};
-  
+  const Back = () => {};
 
-
-    return ( 
-        <div className='App'>
-        <div className='stepButton'>
-          <Button size={'large'} onClick={Restart} type="primary">{'RESTART'}</Button>
-          <Button size={'large'} onClick={Back} type="primary">{'BACK'}</Button>
-          <Button size={'large'} onClick={Next} type="primary">
-            {'NEXT'}
-          </Button>
-        </div>
-    
-            <Paragraph ellipsis={{rows:1,expandable:true,symbol:"more"}}>
-              
-            </Paragraph>
-          
-        
+  return (
+    <div className="App">
+      <div className="banner">测试</div>
+      <div className="stepButton">
+        <Button size={"large"} onClick={Restart} type="primary">
+          {"RESTART"}
+        </Button>
+        <Button size={"large"} onClick={Back} type="primary">
+          {"BACK"}
+        </Button>
+        <Button size={"large"} onClick={Next} type="primary">
+          {"NEXT"}
+        </Button>
       </div>
-     );
-}
- 
-export default Test;
+      <Paragraph
+        ellipsis={{ rows: 1, expandable: true, symbol: "more" }}
+      ></Paragraph>
+    </div>
+  );
+};
 
+export default Test;
