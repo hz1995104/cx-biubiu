b08928033d893da2e83062ba083b6003d48bcdd5
hezhi <zhi.he@aihuishou.com>
2022-01-27 15:31:56 +0800

    feat(2022-01-27): update

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -16,14 +16,13 @@ const sockPort = process.env.WDS_SOCKET_PORT
 
 module.exports = function (proxy, allowedHost) {
   return {
-    //是否绕过主机检查，不建议这么做，可能受到DNS重新连接攻击
     disableHostCheck:
       !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
     compress: true,
     clientLogLevel: 'none',
-    contentBase: paths.appPublic, //当提供静态文件时，告诉服务器从哪个目录中提供内容
+    contentBase: paths.appPublic,
     contentBasePublicPath: paths.publicUrlOrPath,
-    watchContentBase: true, //告诉服务器contentBase选项下的文件，然后监听
+    watchContentBase: true,
     hot: true,
     transportMode: 'ws',
     injectClient: false,
@@ -31,7 +30,7 @@ module.exports = function (proxy, allowedHost) {
     sockPath,
     sockPort,
     publicPath: paths.publicUrlOrPath.slice(0, -1),
-    quiet: true, //除了初始启动信息之外的任何内容都不会被打印到控制台，这也意味着来自 webpack 的错误或警告在控制台不可见
+    quiet: true,
     watchOptions: {
       ignored: ignoredFiles(paths.appSrc)
     },
