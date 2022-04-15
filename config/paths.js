39dcd8f425dcf0f23ed640c90f1136c35b12cc8f
hezhi <zhi.he@aihuishou.com>
2022-04-15 09:17:10 +0800

    feat(2022-04-15): update

--- config/paths.js
+++ config/paths.js
@@ -16,8 +16,6 @@ const publicUrlOrPath = getPublicUrlOrPath(
   process.env.PUBLIC_URL
 )
 
-console.log('publicUrlOrPath', publicUrlOrPath)
-
 const buildPath = process.env.BUILD_PATH || 'build'
 
 const moduleFileExtensions = [
